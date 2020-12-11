9cfa8f750ffe4f6861dfc95f48ad47ed73a63562
Karan <pate1228@sheridancollege.ca>
2020-12-10 21:28:54 -0500

    Interaction with Layout files and recycler view's and layout managers TODO: Aadapter var's to work

--- app/src/main/java/com/profjk/holidaylist/MainActivity.kt
+++ app/src/main/java/com/profjk/holidaylist/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.profjk.holidaylist
 
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.profjk.holidaylist.network.Holiday
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +15,26 @@ class MainActivity : AppCompatActivity() {
     private var countries : Array<String> = arrayOf("Canada", "USA", "Brazil", "Japan", "Netherlands")
     private var countryCodes : Array<String> = arrayOf("CA", "US", "BR", "JP", "NL")
     private var baseUrl = "https://date.nager.at/api/v2/publicholidays/2021/"
+    private lateinit var rvHolidays : RecyclerView
+    //private lateinit var viewAdapter: BookAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var holidayList: MutableList<Holiday>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.rvHolidays = findViewById(R.id.rvHolidays)
+        this.holidayList = mutableListOf()
+      //  private lateinit var viewAdapter: BookAdapter
+       // this.rvHolidays.adapter = this.viewAdapter
+        this.viewManager = LinearLayoutManager(this.applicationContext)
+        this.rvHolidays.layoutManager = this.viewManager
+
+        this.rvHolidays.setHasFixedSize(true)
+        this.rvHolidays.addItemDecoration(DividerItemDecoration(this.applicationContext, DividerItemDecoration.VERTICAL))
+
+      //  bookViewModel = BookViewModel()
 
         this.initializeSpinner()
     }
@@ -23,5 +44,6 @@ class MainActivity : AppCompatActivity() {
         titlesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spnCountries.adapter = titlesAdapter
     }
+    
 
 }
