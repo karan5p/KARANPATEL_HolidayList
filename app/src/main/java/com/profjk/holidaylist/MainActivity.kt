86c39f025990277c6114b1272598cc6adb3ca818
Karan <pate1228@sheridancollege.ca>
2020-12-10 21:43:45 -0500

    HolidayViewModel.kt implimentation and Interaction with MainActivity.kt

--- app/src/main/java/com/profjk/holidaylist/MainActivity.kt
+++ app/src/main/java/com/profjk/holidaylist/MainActivity.kt
@@ -1,49 +1,37 @@
 package com.profjk.holidaylist
-
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.profjk.holidaylist.network.Holiday
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
-
     private var countries : Array<String> = arrayOf("Canada", "USA", "Brazil", "Japan", "Netherlands")
     private var countryCodes : Array<String> = arrayOf("CA", "US", "BR", "JP", "NL")
     private var baseUrl = "https://date.nager.at/api/v2/publicholidays/2021/"
     private lateinit var rvHolidays : RecyclerView
-    //private lateinit var viewAdapter: BookAdapter
+    private lateinit var viewAdapter: HolidayAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var holidayList: MutableList<Holiday>
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.rvHolidays = findViewById(R.id.rvHolidays)
         this.holidayList = mutableListOf()
-      //  private lateinit var viewAdapter: BookAdapter
-       // this.rvHolidays.adapter = this.viewAdapter
+        this.viewAdapter = HolidayAdapter(this.applicationContext,this.holidayList,this)
+        this.rvHolidays.adapter = this.viewAdapter
         this.viewManager = LinearLayoutManager(this.applicationContext)
         this.rvHolidays.layoutManager = this.viewManager
-
         this.rvHolidays.setHasFixedSize(true)
         this.rvHolidays.addItemDecoration(DividerItemDecoration(this.applicationContext, DividerItemDecoration.VERTICAL))
-
-      //  bookViewModel = BookViewModel()
-
+        holidayViewModel = HolidayViewModel()
         this.initializeSpinner()
     }
-
     private fun initializeSpinner(){
         val titlesAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
         titlesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spnCountries.adapter = titlesAdapter
     }
-    
-
 }
---
+++ app/src/main/java/com/profjk/holidaylist/viewmodel/HolidayViewModel.kt
@@ -0,0 +1,25 @@
+package com.profjk.holidaylist.viewmodel
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.profjk.holidaylist.network.Holiday
+import com.profjk.holidaylist.network.HolidaysApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
+class HolidayViewModel : ViewModel() {
+    private val holiday = MutableLiveData<Holiday>()
+    val response: LiveData<Holiday>
+        get() = holiday
+    fun getHolidayInfo(apiUrl: String){
+        viewModelScope.launch{
+            try{
+                val holiday = HolidaysApi.RETROFIT_SERVICE_HOLIDAY.retrieveResponse(apiUrl)
+                  this@HolidayViewModel.holiday.postValue(holiday)
+            }catch (ex: Exception){
+                Log.e("Holidays", ex.localizedMessage)
+            }
+        }
+    }
+}
